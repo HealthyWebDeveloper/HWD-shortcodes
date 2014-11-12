554da25c53b8fc7698e3cf380219b1f6bd16e3cb
Bradford Knowlton <brad@ucanbonline.net>
2014-11-11 19:09:17 -0700

    Initial commit for plugin

---
+++ hwd-shortcodes.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: HWD Shortcodes
+Plugin URI: https://github.com/HealthyWebDeveloper/HWD-shortcodes/
+Description: Add additional customizations to WordPress.
+Version: 1.8.4
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com/
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/HealthyWebDeveloper/HWD-shortcodes/
+GitHub Branch:     master
+*/
